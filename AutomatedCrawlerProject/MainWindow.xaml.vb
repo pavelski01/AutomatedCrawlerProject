ccfe73db7233334510cfaabaea1545bff6f38325
pavelski <pavelski01@gmail.com>
2016-09-03 23:24:02 +0200

    BrowserHelper

--- AutomatedCrawlerProject/MainWindow.xaml.vb
+++ AutomatedCrawlerProject/MainWindow.xaml.vb
@@ -1,6 +1,13 @@
-﻿Partial Public Class MainWindow
+﻿Imports AutomatedCrawlerProject.Helper.BrowserHelper
+
+Partial Public Class MainWindow
     Inherits Window
     Sub New()
         InitializeComponent()
+    End Sub
+    Private Sub ButtonBase_OnClick(sender As Object, e As RoutedEventArgs)
+        Dim ie = CreateMaximizedBrowser()
+        SafeNavigate(ie, "http://niezalezna.pl")
+        Dim a = GetHtmlDocument("Wiadomości, informacje, publicystyka, opinie | niezalezna.pl")
     End Sub
 End Class
